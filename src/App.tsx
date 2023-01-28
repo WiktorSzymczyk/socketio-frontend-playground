b19a683b851e8c1146680e9bcce232d3e248f5c2
Wiktor Szymczyk <74505952+koziu33en@users.noreply.github.com>
2023-01-28 18:50:12 +0000

    Added basic logic for login page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 			<div className='bg-[#141414] w-screen h-screen flex flex-col justify-center items-center pb-[10%]'>
 				<h1 className='font-light text-5xl animate-character'>Hello</h1>
 				<Register />
-				<h1 onClick={() => window.location}></h1>
+				{/* <h1 onClick={() => window.location}></h1> */}
 			</div>
 		</>
 	);
